11e5b20150779dd6df11979bae8ae152d026f19a
Michael Vuong <michael.vuong789@gmail.com>
2020-02-22 20:02:16 +1100

    Updated FlickrSearch to display the images from redux. Added in the Card and FlickrSearch styles to the app.

--- src/flickrsearch/FlickrSearchComponent.jsx
+++ src/flickrsearch/FlickrSearchComponent.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 
 import Search from '@search/Search';
+import Card from '../card/Card';
 
 const FlickrSearchComponent = (props) => {
   const { fetchImages, images, isFetching } = props;
+
+  let flickrImages = [];
+
+  if (images.length > 0) {
+    flickrImages = images.map((image, index) => {
+      const { author, date_taken, link, media: { m }, tags } = image;
+      return (
+        <Card key={`flickr-${index}`}>
+          <img src={m} onClick={() => window.open(`${link}`, "_blank")} />
+          <div className="card-caption">
+            <div>{author}</div>
+            <div>{date_taken}</div>
+            <div>{tags}</div>
+            <a href={link}>Full size</a>
+          </div>
+        </Card>
+      );
+    });
+  }
 
   return (
     <section className="flickr-search">
       <Search
         onChange={text => fetchImages(text)}
       />
+      <section className="flickr-images">
+        {flickrImages}
+      </section>
     </section>
   );
 }
