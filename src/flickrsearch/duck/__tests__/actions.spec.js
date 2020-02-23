4cb24e12e24c9cac69d965fda35f3b9f31d7806c
Michael Vuong <michael.vuong789@gmail.com>
2020-02-23 17:23:37 +1100

    Added in a missing test

--- src/flickrsearch/duck/__tests__/actions.spec.js
+++ src/flickrsearch/duck/__tests__/actions.spec.js
@@ -4,12 +4,20 @@ import * as TYPES from '../types';
 const { RECEIVE_IMAGES, REQUEST_IMAGES } = TYPES;
 
 describe('Testing fsearch actions', () => {
-  it('should create an action to receive posts', () => {
+  it('should create an action to receive images', () => {
     const expectedAction = {
       type: RECEIVE_IMAGES,
       data: 'test',
     }
 
     expect(actions.receiveImages('test')).toEqual(expectedAction);
-  })
-})
+  });
+
+  it('should create an action to request images', () => {
+    const expectedAction = {
+      type: REQUEST_IMAGES,
+    }
+
+    expect(actions.requestImages()).toEqual(expectedAction);
+  });
+});
